287c3437773aaa6637fcbeba23579b7bb29e8483
Brian Runnells <brian.runnells@gmail.com>
2017-06-13 13:50:08 -0500

    initial dotfiles

---
+++ config.fish
@@ -0,0 +1,25 @@
+alias gst "git status"
+alias gfp "git push --force-with-lease origin"
+alias gf "git fetch -p"
+alias gcp "git cherry-pick"
+alias gcpc "git cherry-pick --continue"
+alias gcpa "git cherry-pick --abort"
+alias gfix "git add .; git commit -m "WIP"; git rebase -i head~2"
+alias grbc "git rebase --continue"
+alias grba "git rebase --abort"
+alias grbo "git rebase origin/master"
+alias ga "git add ."
+alias ets "ember test --serve --no-launch"
+
+alias bi "bower install"
+alias ni "npm install"
+
+source ~/.config/fish/nvm-wrapper/nvm.fish
+
+function grb
+  git rebase -i head~$argv
+end
+
+function etf
+  ember t --server --filter="$argv"
+end
