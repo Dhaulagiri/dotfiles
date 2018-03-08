3cba4b3e4213ab4a841ab4f3d366270c4363f99e
Brian Runnells <brian.runnells@gmail.com>
2018-03-08 10:51:20 -0600

    Update config.fish

--- config.fish
+++ config.fish
@@ -16,10 +16,18 @@ alias ni "npm install"
 
 source ~/.config/fish/nvm-wrapper/nvm.fish
 
+function unparanoidify
+  heroku sudo clients:update "dashboard-staging-pr-$argv" --can-skip-paranoid-check true
+end
+
 function grb
   git rebase -i head~$argv
 end
 
 function etf
   ember t --server --filter="$argv"
+end
+
+function yu
+  yarn upgrade $argv --latest; yarn upgrade $argv
 end
