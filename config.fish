78929b6dc1ae8fe00d579d2652acf78858ee4a47
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2024-05-07 06:57:31 -0500

    Update config.fish

--- config.fish
+++ config.fish
@@ -8,22 +8,36 @@ alias gfix "git add .; git commit -m "WIP"; git rebase -i head~2"
 alias grbc "git rebase --continue"
 alias grba "git rebase --abort"
 alias grbo "git rebase origin/master"
+alias grbm "git rebase origin/main"
 alias ga "git add ."
+
 alias ets "ember test --serve --no-launch"
 
 function grb
   git rebase -i head~$argv
 end
 
-function etf
-  ember t --server --filter="$argv"
-end
+# function etf
+#   ember t --server --filter="$argv"
+# end
 
-function yu
-  yarn upgrade $argv --latest; yarn upgrade $argv
-end
+# function yu
+#   yarn upgrade $argv --latest; yarn upgrade $argv
+# end
 
-alias fixcam "sudo killall VDCAssistant"
+# alias fixcam "sudo killall VDCAssistant"
+# alias nvmd "nvm use default"
+
 alias brewu "brew update; brew upgrade; brew cleanup"
+alias bi "brew install $argv"
+alias bui "brew uninstall $argv"
 
-source /usr/local/opt/asdf/asdf.fish
+
+# source ~/.asdf/asdf.fish
+alias agnode "asdf global nodejs $argv"
+alias alnode "asdf local nodejs $argv"
+alias agruby "asdf global ruby $argv"
+alias alruby "asdf local ruby $argv"
+
+alias ci "code-insiders $argv"
+source /usr/local/opt/asdf/libexec/asdf.fish
